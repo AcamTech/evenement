c36c95597cf2118d148977fb13f2894372b75016
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-18 10:33:54 -0400

    Fill in page title.

--- resources/views/Shared/Layouts/MasterAttendee.blade.php
+++ resources/views/Shared/Layouts/MasterAttendee.blade.php
@@ -1,15 +1,36 @@
 @extends('Shared.Layouts.MasterBlank')
 
+@section('title')
+    @parent
+    Home
+@endsection
+
+@section('page_title')
+    {{Auth::user()->first_name}} {{Auth::user()->last_name}} Dashboard
+@stop
+
 @section('menu_title')
-    TODO
+    {{Auth::user()->first_name}} {{Auth::user()->last_name}}
 @endsection
 
 @section('menu_body')
     <ul class="dropdown-menu" role="menu">
         <li>
+            <a href="{{route('showUserHome')}}">
+                <i class="ico ico-home"></i>
+                Home
+            </a>
+        </li>
+        <li class="divider"></li>
+        <li>
+            <a data-href="{{route('showEditUser')}}" data-modal-id="EditUser"
+               class="loadModal editUserModal" href="javascript:void(0);"><span class="icon ico-user"></span>@lang("Top.my_profile")</a>
+        </li>
+        <li class="divider"></li>
+        <li>
             <a href="#">
-                <i class="ico ico-plus"></i>
-                TODO
+                <i class="ico ico-calendar"></i>
+                My Events
             </a>
         </li>
         <li class="divider"></li>
