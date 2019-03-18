89e21995adeae6f7cdeb30cfa0adb168d0256ef9
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-18 08:47:09 -0400

    Fill in is-attendee middleware.

--- app/Http/Middleware/IsAttendee.php
+++ app/Http/Middleware/IsAttendee.php
@@ -8,6 +8,25 @@ use Illuminate\Support\Facades\Auth;
 class IsAttendee
 {
     /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param Guard $auth
+     *
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request $request
@@ -17,7 +36,32 @@ class IsAttendee
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        return response('Unauthorized.', 401);
-//        return $next($request);
+        if (!$this->auth->check()) {
+            return redirect()->guest('login');
+        }
+
+        $id = $this->auth->id();
+        if (is_null($id)) {
+            return response('Authorized but has no id.', 500);
+        }
+
+        $foundUser = User::query()->where('id', $id)->first();
+        if (is_null($foundUser)) {
+            return response('Authorized and has id but could not find user', 500);
+        }
+
+        $isAdmin = false;
+        foreach ($foundUser->roles as $role) {
+            if ($role->name === 'attendee') {
+                $isAdmin = true;
+
+                break;
+            }
+        }
+        if (!$isAdmin) {
+            return response('Unauthorized.', 401);
+        }
+
+        return $next($request);
     }
 }
