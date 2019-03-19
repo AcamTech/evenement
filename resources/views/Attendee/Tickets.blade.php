a73a9f4ed7b25ede096c0e553c53ece19e46541a
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-19 11:32:57 -0400

    More displaying of tickets.

--- resources/views/Attendee/Tickets.blade.php
+++ resources/views/Attendee/Tickets.blade.php
@@ -2,5 +2,10 @@
 
 @section('content')
     <p>Hello, world!!!</p>
-    <p>Attendees: {{count(attendees)}}</p>
+    <p>Attendees: {{count($attendees)}}</p>
+    <ul>
+        @foreach($attendees as $attendee)
+            <li>{{$attendee->email}}</li>
+        @endforeach
+    </ul>
 @endsection
