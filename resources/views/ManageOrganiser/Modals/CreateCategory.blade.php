7b61f23658356a6f9d898701b0411604c7cb2399
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-26 14:28:39 -0400

    Fill in create-category form.

--- resources/views/ManageOrganiser/Modals/CreateCategory.blade.php
+++ resources/views/ManageOrganiser/Modals/CreateCategory.blade.php
@@ -1,24 +1,28 @@
-<div role="dialog"  class="modal fade" style="display: none;">
+<div role="dialog" class="modal fade" style="display: none;">
     {!! Form::open(array('url' => route('postCreateCategory', ['organiser_id' => $organiser->id]), 'class' => 'ajax')) !!}
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header text-center">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h3 class="modal-title">
-                    <i class="ico-calendar"></i>
-                    @lang("Category.create_category")</h3>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <p>Hello, world!</p>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header text-center">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h3 class="modal-title">
+                        <i class="ico-cabinet"></i>
+                        @lang("Category.create_category")</h3>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                {!! Form::label('title', trans("Category.category_title"), ['class' => 'control-label required']) !!}
+                                {!! Form::text('title', Input::old('title'), ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <p>Hello, world!</p>
+                <div class="modal-footer">
+                    {!! Form::button(trans("basic.cancel"), ['class' => 'btn modal-close btn-danger', 'data-dismiss' => 'modal']) !!}
+                    {!! Form::submit(trans("Category.create_category"), ['class' => "btn btn-success"]) !!}
+                </div>
             </div>
         </div>
-        {!! Form::close() !!}
-    </div>
+    {!! Form::close() !!}
 </div>
