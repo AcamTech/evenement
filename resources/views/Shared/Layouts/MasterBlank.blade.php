c13437029bf96a85de2dc6c6e0901086bc7d0f77
Adrian Parker <ihsw.aparker@gmail.com>
2019-03-28 14:46:57 -0400

    Fill in footer.

--- resources/views/Shared/Layouts/MasterBlank.blade.php
+++ resources/views/Shared/Layouts/MasterBlank.blade.php
@@ -97,6 +97,24 @@
         <p>Hello, world!</p>
     </div>
 </div>
+<div class="brand">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 brand-nav">
+                <p>Hello, world!</p>
+            </div>
+            <div class="col-md-2 brand-icon">
+                <object
+                    type="image/svg+xml"
+                    tabindex="-1"
+                    role="img"
+                    data="/assets/images/wmms-blk.svg"
+                    aria-label="Symbol of the Government of Canada"
+                ></object>
+            </div>
+        </div>
+    </div>
+</div>
 
 @include('Shared.Partials.GlobalFooterJS')
 
